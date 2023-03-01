1f80e98e3c4033a564c0a83ab39b9d6301882811
Bibek Sharma <er.bibeksharma@gmail.com>
2023-03-01 17:30:10 +0545

    Login layout was added.

--- app/src/main/java/com/example/lpy/MainActivity.java
+++ app/src/main/java/com/example/lpy/MainActivity.java
@@ -1,38 +1,46 @@
 package com.example.lpy;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
 
 import com.example.lpy.databinding.ActivityMainBinding;
+import com.example.lpy.ui.home.HomeFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
 
     ActivityMainBinding b;
-    private NavController navController;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         b = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(b.getRoot());
-//        b.bottomNavigationView.setSelectedItemId(R.id.homeFragment);
-//        bottomNavigationView = findViewById(R.id.bottomNavigationView);
-//        navController = Navigation.findNavController(this,R.id.frame_layout);
-//        NavigationUI.setupWithNavController(bottomNavigationView, navController);
-//        Navigation.findNavController(this, R.id.mobile_nav);
-        navController = Navigation.findNavController(this, R.id.frame_layout);
-        NavigationUI.setupWithNavController(b.bottomNavigationView, navController);
+        b.bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        new HomeFragment();
 
 
 
+    }
 
-
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+        switch (item.getItemId()){
+            case R.id.homeFragment:
+                fragment = new HomeFragment();
+                break;
+        }
+        return true;
     }
 }
